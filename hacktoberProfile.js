20233aac9bee0ac8c368a47c4b6ece9d374b85dc
Dilan Isuru <60030244+dilanisuru@users.noreply.github.com>
2021-10-01 11:10:45 +0530

    Update hacktoberProfile.js

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+    {
+    name : "K.W.Dilan Isuru",
+    place : "Piliyandala, Sri Lanka",
+    github : "dilanisuru"
   }
 ];
